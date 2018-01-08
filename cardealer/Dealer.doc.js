78e1ec40389d66f9a0915b8f378802af308e979b
Frankie Murdock <frankinstf@gmail.com>
2018-01-08 04:41:03 -0600

    Add files via upload

---
+++ cardealer/Dealer.doc.js
@@ -0,0 +1,27 @@
+
+function myFunction() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("search");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[2];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+
+      }
+    }
+    // else {
+    //   windows.alert("sorry");
+    // }
+  }
+}
+var tax = .08;
+function calculate(price, tax)
+{
+     alert(price.value * tax);
+}
